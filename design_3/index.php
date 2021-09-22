182fe819318155b535fd845d1aac6fa2545db850
Md. Khasrur Rahman <30666985+KhasrurRahman@users.noreply.github.com>
2021-09-22 09:09:22 +0600

    asdads

--- design_3/index.php
+++ design_3/index.php
@@ -4,7 +4,6 @@
 
         <!-- <h1 style="text-transform: uppercase">Training Through Excellence</h1> -->
         <div class="video-container">
-            <div class="color-overlay"></div>
             <video autoplay loop muted width="100%">
                 <source src="assets/video/bd_army.mp4" type="video/mp4">
             </video>
