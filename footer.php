2a62035fa3a900520983f5dfb915790b9be14951
Md. Khasrur Rahman <khasrur8@gmail.com>
2021-11-21 01:23:52 +0600

    asdasd

--- footer.php
+++ footer.php
@@ -5,7 +5,7 @@
             <div class="col-lg-3 col-md-6 col-sm-6">
                 <div class="single-footer-widget">
                     <div class="logo">
-                        <img src="assets/img/logo.png" alt="logo" style="max-width: 100px">
+                        <img src="/AFD/assets/img/logo.png" alt="logo" style="max-width: 100px">
                         <p>ARMED FORCES DIVISION TRAINING INSTITUTIONS<br>
                             infotraining.com<br>
                             +8801769014368 (Joint Training)</p>
@@ -80,35 +80,35 @@
 
 <div class="go-top"><i class="fas fa-arrow-up"></i></div>
 
-<script src="assets/js/jquery.min.js"></script>
+<script src="/AFD/assets/js/jquery.min.js"></script>
 
-<script src="assets/js/popper.min.js"></script>
+<script src="/AFD/assets/js/popper.min.js"></script>
 
-<script src="assets/js/bootstrap.min.js"></script>
+<script src="/AFD/assets/js/bootstrap.min.js"></script>
 
-<script src="assets/js/jquery.meanmenu.js"></script>
+<script src="/AFD/assets/js/jquery.meanmenu.js"></script>
 
-<script src="assets/js/jquery.magnific-popup.min.js"></script>
+<script src="/AFD/assets/js/jquery.magnific-popup.min.js"></script>
 
-<script src="assets/js/owl.carousel.min.js"></script>
+<script src="/AFD/assets/js/owl.carousel.min.js"></script>
 
-<script src="assets/js/parallax.min.js"></script>
+<script src="/AFD/assets/js/parallax.min.js"></script>
 
-<script src="assets/js/jquery.nice-select.min.js"></script>
+<script src="/AFD/assets/js/jquery.nice-select.min.js"></script>
 
-<script src="assets/js/wow.min.js"></script>
+<script src="/AFD/assets/js/wow.min.js"></script>
 
-<script src="assets/js/jquery.ajaxchimp.min.js"></script>
+<script src="/AFD/assets/js/jquery.ajaxchimp.min.js"></script>
 
-<script src="assets/js/form-validator.min.js"></script>
+<script src="/AFD/assets/js/form-validator.min.js"></script>
 
-<script src="assets/js/contact-form-script.js"></script>
+<script src="/AFD/assets/js/contact-form-script.js"></script>
 
-<script src="assets/js/bahama-map.js"></script>
+<script src="/AFD/assets/js/bahama-map.js"></script>
 
-<script src="assets/js/main.js"></script>
-<script src="assets/js/blue-slider.js"></script>
-<script src="assets/js/slider.js"></script>
+<script src="/AFD/assets/js/main.js"></script>
+<script src="/AFD/assets/js/blue-slider.js"></script>
+<script src="/AFD/assets/js/slider.js"></script>
 <script>
     $(document).ready(function() {
         $('.collapse.in').prev('.panel-heading').addClass('active');
--- header.php
+++ header.php
@@ -7,30 +7,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1,
             shrink-to-fit=no">
 
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/AFD/assets/css/bootstrap.min.css">
 
-    <link rel="stylesheet" href="assets/css/animate.min.css">
+    <link rel="stylesheet" href="/AFD/assets/css/animate.min.css">
 
-    <link rel="stylesheet" href="assets/css/fontawesome.min.css">
+    <link rel="stylesheet" href="/AFD/assets/css/fontawesome.min.css">
 
-    <link rel="stylesheet" href="assets/css/magnific-popup.min.css">
-    <link rel="stylesheet" href="assets/css/flaticon.css">
+    <link rel="stylesheet" href="/AFD/assets/css/magnific-popup.min.css">
+    <link rel="stylesheet" href="/AFD/assets/css/flaticon.css">
 
-    <link rel="stylesheet" href="assets/css/nice-select.min.css">
+    <link rel="stylesheet" href="/AFD/assets/css/nice-select.min.css">
 
-    <link rel="stylesheet" href="assets/css/meanmenu.css">
+    <link rel="stylesheet" href="/AFD/assets/css/meanmenu.css">
 
-    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="/AFD/assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="/AFD/assets/css/style.css">
 
-    <link rel="stylesheet" href="assets/css/responsive.css">
-    <link rel="stylesheet" href="assets/css/fontello.css">
-    <link rel="stylesheet" href="assets/css/slider.min.css">
+    <link rel="stylesheet" href="/AFD/assets/css/responsive.css">
+    <link rel="stylesheet" href="/AFD/assets/css/fontello.css">
+    <link rel="stylesheet" href="/AFD/assets/css/slider.min.css">
     <title>ARMED FORCES DIVISION</title>
-    <link rel="icon" type="image/png" href="assets/img/logo_2.png">
-    <link rel="stylesheet" href="assets/dist/css/lightbox.min.css">
+    <link rel="icon" type="image/png" href="/AFD/assets/img/logo_2.png">
+    <link rel="stylesheet" href="/AFD/assets/dist/css/lightbox.min.css">
     <!-- favicon -->
-    <link rel="icon" type="image/png" href="assets/img/logo.png" />
+    <link rel="icon" type="image/png" href="/AFD/assets/img/logo.png" />
 
 </head>
 
@@ -44,8 +44,8 @@
         <div class="navbar-area">
             <div class="bahama-mobile-nav">
                 <div class="logo">
-                    <a href="index.php">
-                        <img src="assets/img/logo.png" alt="logo" style="max-width: 85px">
+                    <a href="/AFD/index.php">
+                        <img src="/assets/img/logo.png" alt="logo" style="max-width: 85px">
                     </a>
 
                     <span class="logo_text">Training Through Excellence</span>
@@ -54,8 +54,8 @@
             <div class="bahama-nav">
                 <div class="container">
                     <nav class="navbar navbar-expand-md navbar-light">
-                        <a class="navbar-brand" href="index.php">
-                            <img src="assets/img/logo.png" alt="logo" style="max-width: 80px">
+                        <a class="navbar-brand" href="/AFD/index.php">
+                            <img src="/assets/img/logo.png" alt="logo" style="max-width: 80px">
                         </a>
                         <p style="color: white;
                                 font-size: 20px;
@@ -68,19 +68,19 @@
                         <div class="collapse navbar-collapse mean-menu" id="navbarSupportedContent">
                             <ul class="navbar-nav">
 
-                                <li class="nav-item"><a href="index.php" class="nav-link">Home</a></li>
+                                <li class="nav-item"><a href="/AFD/index.php" class="nav-link">Home</a></li>
 
                                 <li class="nav-item"><a href="#" class="nav-link">Training
                                         Institutions <i class="fas
                                                 fa-chevron-down"></i></a>
                                     <ul class="dropdown-menu">
-                                        <li class="nav-item"><a href="school_list.php" class="nav-link">BANGLADESH
+                                        <li class="nav-item"><a href="/AFD/school_list.php" class="nav-link">BANGLADESH
                                                 ARMY</a></li>
-                                        <li class="nav-item"><a href="navy_list.php" class="nav-link">BANGLADESH
+                                        <li class="nav-item"><a href="/AFD/navy_list.php" class="nav-link">BANGLADESH
                                                 NAVY</a></li>
-                                        <li class="nav-item"><a href="air_force_list.php" class="nav-link"> BANGLADESH
+                                        <li class="nav-item"><a href="/AFD/air_force_list.php" class="nav-link"> BANGLADESH
                                                 AIR FORCE</a></li>
-                                        <li class="nav-item"><a href="tri_list.php" class="nav-link">TRI SERVICE
+                                        <li class="nav-item"><a href="/AFD/tri_list.php" class="nav-link">TRI SERVICE
                                                 INSTITUTION</a></li>
                                     </ul>
                                 </li>
