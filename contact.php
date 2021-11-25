39357e98ae9995da6c6122a532045c070d7bc8f1
labony277 <45601560+labony277@users.noreply.github.com>
2021-11-25 10:29:03 +0600

    update contact

--- contact.php
+++ contact.php
@@ -5,274 +5,141 @@
     .course:hover {
         background-color: #265D3B;
     }
+
+    .styled-table {
+    border-collapse: collapse;
+    margin: 25px 0;
+    font-size: 0.9em;
+    font-family: sans-serif;
+    min-width: 100%;
+    box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
+  }
+
+  .styled-table thead tr {
+    background-color: #265D3B;
+    color: #ffffff;
+    text-align: left;
+  }
+
+  .styled-table th,
+  .styled-table td {
+    padding: 12px 15px;
+  }
+
+  .styled-table tbody tr {
+    border-bottom: 1px solid #dddddd;
+  }
+
+  .styled-table tbody tr:nth-of-type(even) {
+    background-color: #f3f3f3;
+  }
+
+  .styled-table tbody tr:last-of-type {
+    border-bottom: 2px solid #265D3B;
+  }
+
+  .styled-table tbody tr.active-row {
+    font-weight: bold;
+    color: #009879;
+  }
 </style>
 <section class="space-ptb course-list bg-light">
-    <div class="container">
-        <div class="row justify-content-center ">
-
-
-            <div class="course col-md-8 text-center">
-                <div class="no-gutters box-shadow mb-4">
-
-                    <div class="course-info p-0 h-100">
-                        <div class="px-4 pt-4">
-                            <div class="course-title">
-                                <a href="#"><strong>Principal&nbsp;Staff Officer's office</strong></a>
-                            </div>
-                            <p>
-                                Appointment: Private Secretary to Principal&nbsp;Staff Officer<br>
-                                Rank: Major<br>
-                                Name: <span><span><span>Md Al-Amin, SUP, BIR</span></span></span><br>
-                                Telephone: 983-4322&nbsp;<br>
-                                Mobile: 0176901-4322<br>
-                                E-mail: ps_pso@afd.gov.bd , ps2psoafd@gmail.com</p>
-                        </div>
-
-                    </div>
-
-                </div>
-
-            </div>
-
-
-            <div class="course col-md-8 text-center">
-                <div class="no-gutters box-shadow mb-4">
-
-                    <div class="course-info p-0 h-100">
-                        <div class="px-4 pt-4">
-                            <div class="course-title">
-                                <a href="#"><strong>Principal&nbsp;Staff Officer's office</strong></a>
-                            </div>
-                            <p>
-                                Appointment: Private Secretary to Principal&nbsp;Staff Officer<br>
-                                Rank: Major<br>
-                                Name: <span><span><span>Md Al-Amin, SUP, BIR</span></span></span><br>
-                                Telephone: 983-4322&nbsp;<br>
-                                Mobile: 0176901-4322<br>
-                                E-mail: ps_pso@afd.gov.bd , ps2psoafd@gmail.com</p>
-                        </div>
-
-                    </div>
-
-                </div>
-
-            </div>
-
-
-            <div class="course col-md-8 text-center">
-                <div class="no-gutters box-shadow mb-4">
-
-                    <div class="course-info p-0 h-100">
-                        <div class="px-4 pt-4">
-                            <div class="course-title">
-                                <a href="#"><strong>Principal&nbsp;Staff Officer's office</strong></a>
-                            </div>
-                            <p>
-                                Appointment: Private Secretary to Principal&nbsp;Staff Officer<br>
-                                Rank: Major<br>
-                                Name: <span><span><span>Md Al-Amin, SUP, BIR</span></span></span><br>
-                                Telephone: 983-4322&nbsp;<br>
-                                Mobile: 0176901-4322<br>
-                                E-mail: ps_pso@afd.gov.bd , ps2psoafd@gmail.com</p>
-                        </div>
-
-                    </div>
-
-                </div>
-
-            </div>
-
-
-            <div class="course col-md-8 text-center">
-                <div class="no-gutters box-shadow mb-4">
-
-                    <div class="course-info p-0 h-100">
-                        <div class="px-4 pt-4">
-                            <div class="course-title">
-                                <a href="#"><strong>Principal&nbsp;Staff Officer's office</strong></a>
-                            </div>
-                            <p>
-                                Appointment: Private Secretary to Principal&nbsp;Staff Officer<br>
-                                Rank: Major<br>
-                                Name: <span><span><span>Md Al-Amin, SUP, BIR</span></span></span><br>
-                                Telephone: 983-4322&nbsp;<br>
-                                Mobile: 0176901-4322<br>
-                                E-mail: ps_pso@afd.gov.bd , ps2psoafd@gmail.com</p>
-                        </div>
-
-                    </div>
-
-                </div>
-
-            </div>
-
-
-            <div class="course col-md-8 text-center">
-                <div class="no-gutters box-shadow mb-4">
-
-                    <div class="course-info p-0 h-100">
-                        <div class="px-4 pt-4">
-                            <div class="course-title">
-                                <a href="#"><strong>Principal&nbsp;Staff Officer's office</strong></a>
-                            </div>
-                            <p>
-                                Appointment: Private Secretary to Principal&nbsp;Staff Officer<br>
-                                Rank: Major<br>
-                                Name: <span><span><span>Md Al-Amin, SUP, BIR</span></span></span><br>
-                                Telephone: 983-4322&nbsp;<br>
-                                Mobile: 0176901-4322<br>
-                                E-mail: ps_pso@afd.gov.bd , ps2psoafd@gmail.com</p>
-                        </div>
-
-                    </div>
-
-                </div>
-
-            </div>
-
-
-            <div class="course col-md-8 text-center">
-                <div class="no-gutters box-shadow mb-4">
-
-                    <div class="course-info p-0 h-100">
-                        <div class="px-4 pt-4">
-                            <div class="course-title">
-                                <a href="#"><strong>Principal&nbsp;Staff Officer's office</strong></a>
-                            </div>
-                            <p>
-                                Appointment: Private Secretary to Principal&nbsp;Staff Officer<br>
-                                Rank: Major<br>
-                                Name: <span><span><span>Md Al-Amin, SUP, BIR</span></span></span><br>
-                                Telephone: 983-4322&nbsp;<br>
-                                Mobile: 0176901-4322<br>
-                                E-mail: ps_pso@afd.gov.bd , ps2psoafd@gmail.com</p>
-                        </div>
-
-                    </div>
-
-                </div>
-
-            </div>
-
-
-            <div class="course col-md-8 text-center">
-                <div class="no-gutters box-shadow mb-4">
-
-                    <div class="course-info p-0 h-100">
-                        <div class="px-4 pt-4">
-                            <div class="course-title">
-                                <a href="#"><strong>Principal&nbsp;Staff Officer's office</strong></a>
-                            </div>
-                            <p>
-                                Appointment: Private Secretary to Principal&nbsp;Staff Officer<br>
-                                Rank: Major<br>
-                                Name: <span><span><span>Md Al-Amin, SUP, BIR</span></span></span><br>
-                                Telephone: 983-4322&nbsp;<br>
-                                Mobile: 0176901-4322<br>
-                                E-mail: ps_pso@afd.gov.bd , ps2psoafd@gmail.com</p>
-                        </div>
-
-                    </div>
-
-                </div>
-
-            </div>
-
-            <div class="course col-md-8 text-center">
-                <div class="no-gutters box-shadow mb-4">
-
-                    <div class="course-info p-0 h-100">
-                        <div class="px-4 pt-4">
-                            <div class="course-title">
-                                <a href="#"><strong>Principal&nbsp;Staff Officer's office</strong></a>
-                            </div>
-                            <p>
-                                Appointment: Private Secretary to Principal&nbsp;Staff Officer<br>
-                                Rank: Major<br>
-                                Name: <span><span><span>Md Al-Amin, SUP, BIR</span></span></span><br>
-                                Telephone: 983-4322&nbsp;<br>
-                                Mobile: 0176901-4322<br>
-                                E-mail: ps_pso@afd.gov.bd , ps2psoafd@gmail.com</p>
-                        </div>
-
-                    </div>
-
-                </div>
-
-            </div>
-
-            <div class="course col-md-8 text-center">
-                <div class="no-gutters box-shadow mb-4">
-
-                    <div class="course-info p-0 h-100">
-                        <div class="px-4 pt-4">
-                            <div class="course-title">
-                                <a href="#"><strong>Principal&nbsp;Staff Officer's office</strong></a>
-                            </div>
-                            <p>
-                                Appointment: Private Secretary to Principal&nbsp;Staff Officer<br>
-                                Rank: Major<br>
-                                Name: <span><span><span>Md Al-Amin, SUP, BIR</span></span></span><br>
-                                Telephone: 983-4322&nbsp;<br>
-                                Mobile: 0176901-4322<br>
-                                E-mail: ps_pso@afd.gov.bd , ps2psoafd@gmail.com</p>
-                        </div>
-
-                    </div>
-
-                </div>
-
-            </div>
-
-            <div class="course col-md-8 text-center">
-                <div class="no-gutters box-shadow mb-4">
-
-                    <div class="course-info p-0 h-100">
-                        <div class="px-4 pt-4">
-                            <div class="course-title">
-                                <a href="#"><strong>Principal&nbsp;Staff Officer's office</strong></a>
-                            </div>
-                            <p>
-                                Appointment: Private Secretary to Principal&nbsp;Staff Officer<br>
-                                Rank: Major<br>
-                                Name: <span><span><span>Md Al-Amin, SUP, BIR</span></span></span><br>
-                                Telephone: 983-4322&nbsp;<br>
-                                Mobile: 0176901-4322<br>
-                                E-mail: ps_pso@afd.gov.bd , ps2psoafd@gmail.com</p>
-                        </div>
-
-                    </div>
-
-                </div>
-
-            </div>
-
-            <div class="course col-md-8 text-center">
-                <div class="no-gutters box-shadow mb-4">
-
-                    <div class="course-info p-0 h-100">
-                        <div class="px-4 pt-4">
-                            <div class="course-title">
-                                <a href="#"><strong>Principal&nbsp;Staff Officer's office</strong></a>
-                            </div>
-                            <p>
-                                Appointment: Private Secretary to Principal&nbsp;Staff Officer<br>
-                                Rank: Major<br>
-                                Name: <span><span><span>Md Al-Amin, SUP, BIR</span></span></span><br>
-                                Telephone: 983-4322&nbsp;<br>
-                                Mobile: 0176901-4322<br>
-                                E-mail: ps_pso@afd.gov.bd , ps2psoafd@gmail.com</p>
-                        </div>
-
-                    </div>
-
-                </div>
-
-            </div>
-
+   
+    <div class="container ">
+        <div class="row">
+          <div class="col-md-12">
+      
+            <table class="styled-table">
+              <thead>
+                <tr>
+                  <th>SER</th>
+                  <th>Rank And Name​</th>
+                  <th>Appoinment</th>
+                  <th>Email and Mobile Number</th>
+                </tr>
+              </thead>
+              <tbody>
+              <tr>
+                  <td>1​</td>
+                  <td>Air Commodore Md Sharif  
+                    Uddin Sarker, GUP, BPP,  
+                    nde, psc, GD(P).</td>
+                  <td> DIRECTOR GENERAL TRAINING</td>
+                  <td>dg_trg@afd.gov.bd <br>
+                    01769014360
+                  </td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>Colonel Mohammed  
+                        Rezaul Karim, afwe, psc, G </td>
+                    <td>COLONE STAFF TRAINING</td>
+                    <td>col_trg@afd.gov.bd <br>
+                        01769014314
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>3​</td>
+                    <td>Lieutenant Colonel AKM  
+                        Kayes, afwc, psc, AC.</td>
+                    <td>GSO-1 (JOINT&MISC
+                        TRAINING) </td>
+                    <td>gso1_misc@afd.gov.bd <br>
+                        01716014362
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>4​</td>
+                    <td>Commander Shah Kamruzzaman, (ND), psc,BN.</td>
+                    <td> GSO-1 (NAVAL
+                        TRAINING)</td>
+                    <td>gso1_nat@afd.gov.bd <br>
+                        01769702904
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>5</td>
+                    <td>Lieutenant Colonel Jeenia  Haque, psc, Sigs.</td>
+                    <td> GSo1-  (ARMY
+                        TRAINING)</td>
+                    <td>gso1_at@afd.gov.bd <br>
+                        01769014364
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>6​</td>
+                    <td>Wing Commander Monjila 
+                        Rizwan, psc, Met .</td>
+                    <td> GSO-1 (AIR
+                        TRAINING)</td>
+                    <td>gso1_airt@afd.gov.bd <br>
+                        01769993661
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>7​</td>
+                    <td>Major Md Joynul Abedin,psc, Arty.</td>
+                    <td> GSO-2(JOINT TRAINING)</td>
+                    <td>Gso2_jttrg@afd.gov.bd <br>
+                        01769014368
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>8​</td>
+                    <td>Lieutenant Commander Mohammad Emdad Ullah Mian, (ND), PCGMS, psc,BN  .</td>
+                    <td> GSO-2(JOINT &
+                        MISC TRAINING)</td>
+                    <td>gso1_jtmisctrg@afd.gov.bd <br>
+                        01769014353
+                    </td>
+                  </tr>
+               
+              </tbody>
+            </table>
+          </div>
         </div>
-    </div>
-    </div>
+      </div>
+      
 </section>
 
 
