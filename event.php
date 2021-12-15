9355f1d978ae62f9353724ecf3f77a3bbbdfbb24
labony277 <45601560+labony277@users.noreply.github.com>
2021-12-15 09:01:01 +0600

    update v7

--- event.php
+++ event.php
@@ -1,114 +1,160 @@
 <?php include 'header.php'; ?>
-<style>
-    @import url(https://fonts.googleapis.com/css?family=Open+Sans:400,300,600);	
 
-html {
-  border-top: 5px solid #fff;
-  background: #58DDAF;
-  color: #2a2a2a;
-}
+    <!-- new section -->
+    <section class="our-coverage-area ptb-100">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-6 col-md-12">
+                    <div class="coverage-area-content">
+                        <!-- <span class="sub-title"><i
+                                class="flaticon-care-about-environment"></i>
+                            About HCiD</span> -->
+                        <h2>Awareness on Healthcare in Danger (HCiD)</h2>
+                        <p>Health care in Danger is a very contemporary and new subject and ICRC guidance on this issue was launched very recently on 31 Mar 2021. So Bangladesh Armed Forces is the forerunner to conduct a seminar on this contemporary subject. Awareness on Healthcare in Danger (HCiD) is an initiative of the International Red Cross and Red Crescent movement aimed at address the issue of violence against patients, health workers, facilities and vehicle, and ensuring safe access to deliver healthcare in armed conflict and other emergencies. HCiD support the implementation of concrete, practical measures and operational responses at National and local levels to prevent violence and safeguard health care in armed conflict and other emergencies. </p>
+                        <p>ICRC and Bangladesh Armed Forces have a long history of cooperation. On 25 November, Training Directorate, Armed Forces Division and ICRC jointly organized a seminar. <br>
+                          Speakers of the seminar were: <br>
+                          (1)	Mr. Henning Krause, Protection Coordinator, ICRC, Bangladesh.
+                          (2)	Brig Gen Md Shahinur Farhad, psc  from Bangladesh Institute of Peace Support Operation Training (BIPSOT).
+                          </p>
+                        
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-12">
+                    <div class="coverage-area-map">
+                        <div id="carouselExampleIndicators" class="carousel
+                            slide"
+                            data-ride="carousel">
+                            <!-- <ol class="carousel-indicators">
+                                <li data-target="#carouselExampleIndicators"
+                                    data-slide-to="0"
+                                    class="active"></li>
+                                <li data-target="#carouselExampleIndicators"
+                                    data-slide-to="1"></li>
+                                <li data-target="#carouselExampleIndicators"
+                                    data-slide-to="2"></li>
+                                    <li data-target="#carouselExampleIndicators"
+                                    data-slide-to="3"></li>
+                                    <li data-target="#carouselExampleIndicators"
+                                    data-slide-to="4"></li>
+                            </ol> -->
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <img class="d-block w-100"
+                                        src="../AFD/assets/img/Event/HCiD/AFD_7433.JPG"
+                                        alt="First slide">
+                                </div>
+                                <div class="carousel-item">
+                                    <img class="d-block w-100"
+                                        src="../AFD/assets/img/Event/HCiD/AFD_7469.JPG"
+                                        alt="Second slide">
+                                </div>
+                                <div class="carousel-item">
+                                    <img class="d-block w-100"
+                                        src="../AFD/assets/img/Event/HCiD/AFD_7482.JPG"
+                                        alt="Third slide">
+                                </div>
+                                <div class="carousel-item">
+                                    <img class="d-block w-100"
+                                        src="../AFD/assets/img/Event/HCiD/AFD_7520.JPG"
+                                        alt="Four slide">
+                                </div>
+                                <div class="carousel-item">
+                                    <img class="d-block w-100"
+                                        src="../AFD/assets/img/Event/HCiD/AFD_7546.JPG"
+                                        alt="Five slide">
+                                </div>
+                            </div>
+                            <a class="carousel-control-prev"
+                                href="#carouselExampleIndicators"
+                                role="button" data-slide="prev">
+                                <span class="carousel-control-prev-icon"
+                                    aria-hidden="true"></span>
+                                <span class="sr-only">Previous</span>
+                            </a>
+                            <a class="carousel-control-next"
+                                href="#carouselExampleIndicators"
+                                role="button" data-slide="next">
+                                <span class="carousel-control-next-icon"
+                                    aria-hidden="true"></span>
+                                <span class="sr-only">Next</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <br>
+    <!-- end new -->
+    <!-- new2 -->
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-6 col-md-12">
+                <div class="about-image">
+                    <div id="carouselExampleControls" class="carousel slide"
+                        data-ride="carousel">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img class="d-block w-100" src="../AFD/assets/img/Event/Kabadi/AH7A6059.JPG" alt="First
+                                    slide">
+                            </div>
+                            <div class="carousel-item">
+                                <img class="d-block w-100" src="../AFD/assets/img/Event/Kabadi/AH7A6066.JPG" alt="Second
+                                    slide">
+                            </div>
+                            <div class="carousel-item">
+                                <img class="d-block w-100" src="../AFD/assets/img/Event/Kabadi/AH7A6069.JPG" alt="Third
+                                    slide">
+                            </div>
+                            <div class="carousel-item">
+                                <img class="d-block w-100" src="../AFD/assets/img/Event/Kabadi/AH7A6454.JPG" alt="Third
+                                    slide">
+                            </div>
+                            <div class="carousel-item">
+                                <img class="d-block w-100" src="../AFD/assets/img/Event/Kabadi/AH7A6531.JPG" alt="Third
+                                    slide">
+                            </div>
+                            <div class="carousel-item">
+                                <img class="d-block w-100" src="../AFD/assets/img/Event/Kabadi/DSC_7244.JPG" alt="Third
+                                    slide">
+                            </div>
+                            <div class="carousel-item">
+                                <img class="d-block w-100" src="../AFD/assets/img/Event/Kabadi/DSC_7250.JPG" alt="Third
+                                    slide">
+                            </div>
+                        </div>
+                        <a class="carousel-control-prev"
+                            href="#carouselExampleControls" role="button"
+                            data-slide="prev">
+                            <span class="carousel-control-prev-icon"
+                                aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="carousel-control-next"
+                            href="#carouselExampleControls" role="button"
+                            data-slide="next">
+                            <span class="carousel-control-next-icon"
+                                aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </div>
+                </div>
 
-html, body {
-  margin: 0;
-  padding: 0;
-  font-family: 'Open Sans';
-}
+            </div>
+            <div class="col-lg-6 col-md-12">
+                <div class="about-content">
+                    <!-- <span class="sub-title"><i
+                            class="flaticon-care-about-environment"></i> About
+                        MUJIB BORSHO INTER SERVICE KABADI COMPETITION – 2021</span> -->
+                    <h5>MUJIB BORSHO INTER SERVICE KABADI COMPETITION – 2021</h5>
+                    <p>'Mujib Borsho Inter Service Kabadi Competition-2021’ was held from 29 November 2021 to 02 December 2021 at BAF Base Birsreshtho Matiur Rahman, Jashore. Bangladesh Army, Bangladesh Navy and Bangladesh Air Force Kabadi teams participated in the competition. Rear Admiral M Anwar Hossain (ND), NGP, ndc, afwc, psc, BN, Commander Khulna Naval Area inaugurated the competition on 29 November 2021. Bangladesh Navy became Champion and Bangladesh Air Force became Runner-up. M Tuhin Tarafdar, L Top, Captain of Bangladesh Navy team adjudged as the best player of the Competition.  </p>
+                    <p> Air Vice Marshal A H M Fazlul Haque, BBP, BSP, ndu, afwc, psc, Assistant Chief of Air Staff (Plans) Bangladesh Air Force handed over the trophies to the winner, runner-up and the best player as Chief Guest of the Closing Ceremony.
 
-h1 {
-  color: black;
-  
-  font-weight: 300;
-  margin-left: 40px;
-}
-
-#slider {
-  position: relative;
-  overflow: hidden;
-  margin: 20px auto 0 auto;
-  border-radius: 4px;
-  margin-right: 60px;
-}
-
-#slider ul {
-  position: relative;
-  margin: 0;
-  padding: 0;
-  height: 200px;
-  list-style: none;
-}
-
-#slider ul li {
-  position: relative;
-  display: block;
-  float: left;
-  margin: 0;
-  padding: 0;
-  width: 500px;
-  height: 300px;
-  background: #ccc;
-  text-align: center;
-  line-height: 300px;
-}
-
-a.control_prev, a.control_next {
-  position: absolute;
-  top: 40%;
-  z-index: 999;
-  display: block;
-  padding: 4% 3%;
-  width: auto;
-  height: auto;
-  background: #2a2a2a;
-  color: #fff;
-  text-decoration: none;
-  font-weight: 600;
-  font-size: 18px;
-  opacity: 0.8;
-  cursor: pointer;
-}
-
-a.control_prev:hover, a.control_next:hover {
-  opacity: 1;
-  -webkit-transition: all 0.2s ease;
-}
-
-a.control_prev {
-  border-radius: 0 2px 2px 0;
-}
-
-a.control_next {
-  right: 0;
-  border-radius: 2px 0 0 2px;
-}
-
-.slider_option {
-  position: relative;
-  margin: 10px auto;
-  width: 160px;
-  font-size: 18px;
-}
-
-</style>
-<section>
-    <h1>Incredibly Basic Slider</h1>
-    <div id="slider">
-      <a href="#" class="control_next">></a>
-      <a href="#" class="control_prev"><</a>
-      <ul>
-        <li> <img  src="assets/img/school/navy/bna/1.jpg" alt="First slide"></li>
-        <li style="background: #aaa;">SLIDE 2</li>
-        <li>SLIDE 3</li>
-        <li style="background: #aaa;">SLIDE 4</li>
-      </ul>  
+                    </p>
+                </div>
+            </div>
+        </div>
     </div>
-    
-    <!-- <div class="slider_option">
-      <input type="checkbox" id="checkbox">
-      <label for="checkbox">Autoplay Slider</label>
-    </div>  -->
-    
-</section>
-<br>
+   <br>
 
-
-<?php include 'footer.php'; ?>
+    <?php include 'footer.php'; ?>
