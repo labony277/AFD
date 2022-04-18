2d253840e1e6d2dd560ea499ac8f2a657f7f6737
labony277 <45601560+labony277@users.noreply.github.com>
2022-04-18 10:40:46 +0600

    update event (18/4/22)

--- event.php
+++ event.php
@@ -9,16 +9,6 @@
                         <!-- <span class="sub-title"><i
                                 class="flaticon-care-about-environment"></i>
                             About HCiD</span> -->
-<<<<<<< HEAD
-                        <h2>Awareness on Healthcare in Danger (HCiD)</h2>
-                        <p>Health care in Danger is a very contemporary and new subject and ICRC guidance on this issue was launched very recently on 31 Mar 2021. So Bangladesh Armed Forces is the forerunner to conduct a seminar on this contemporary subject. Awareness on Healthcare in Danger (HCiD) is an initiative of the International Red Cross and Red Crescent movement aimed at address the issue of violence against patients, health workers, facilities and vehicle, and ensuring safe access to deliver healthcare in armed conflict and other emergencies. HCiD support the implementation of concrete, practical measures and operational responses at National and local levels to prevent violence and safeguard health care in armed conflict and other emergencies. </p>
-                        <p>ICRC and Bangladesh Armed Forces have a long history of cooperation. On 25 November, Training Directorate, Armed Forces Division and ICRC jointly organized a seminar. <br>
-                          Speakers of the seminar were: <br>
-                          (1)	Mr. Henning Krause, Protection Coordinator, ICRC, Bangladesh.
-                          (2)	Brig Gen Md Shahinur Farhad, psc  from Bangladesh Institute of Peace Support Operation Training (BIPSOT).
-                          </p>
-                        
-=======
                         <h2 style="text-align: justify;">Seminar On Awareness on
                             Healthcare in Danger (HCiD)</h2>
                         <p style="text-align: justify;">Health care in Danger is
@@ -50,7 +40,6 @@
                             (BIPSOT).
                         </p>
 
->>>>>>> c7f02b2256662a67b3105bd4386ad783400e1d1a
                     </div>
                 </div>
                 <div class="col-lg-6 col-md-12">
@@ -74,88 +63,26 @@
                             <div class="carousel-inner">
                                 <div class="carousel-item active">
                                     <img class="d-block w-100"
-<<<<<<< HEAD
-                                        src="../assets/img/Event/HCiD/AFD_7433-min.JPG"
-=======
                                         src="../AFD/assets/img/Event/HCiD/AFD_7433-min.JPG"
->>>>>>> c7f02b2256662a67b3105bd4386ad783400e1d1a
                                         alt="First slide">
                                 </div>
                                 <div class="carousel-item">
                                     <img class="d-block w-100"
-<<<<<<< HEAD
-                                        src="../assets/img/Event/HCiD/AFD_7469-min.JPG"
-=======
                                         src="../AFD/assets/img/Event/HCiD/AFD_7469-min.JPG"
->>>>>>> c7f02b2256662a67b3105bd4386ad783400e1d1a
                                         alt="Second slide">
                                 </div>
                                 <div class="carousel-item">
                                     <img class="d-block w-100"
-<<<<<<< HEAD
-                                        src="../assets/img/Event/HCiD/AFD_7482-min.JPG"
-=======
                                         src="../AFD/assets/img/Event/HCiD/AFD_7482-min.JPG"
->>>>>>> c7f02b2256662a67b3105bd4386ad783400e1d1a
                                         alt="Third slide">
                                 </div>
                                 <div class="carousel-item">
                                     <img class="d-block w-100"
-<<<<<<< HEAD
-                                        src="../assets/img/Event/HCiD/AFD_7520-min.JPG"
-=======
                                         src="../AFD/assets/img/Event/HCiD/AFD_7520-min.JPG"
->>>>>>> c7f02b2256662a67b3105bd4386ad783400e1d1a
                                         alt="Four slide">
                                 </div>
                                 <div class="carousel-item">
                                     <img class="d-block w-100"
-<<<<<<< HEAD
-                                        src="../assets/img/Event/HCiD/AFD_7546-min.JPG"
-                                        alt="Five slide">
-                                </div>
-                                <div class="carousel-item">
-                                  <img class="d-block w-100"
-                                      src="../assets/img/Event/HCiD/AFD_7550-min.JPG"
-                                      alt="Five slide">
-
-                              </div>
-                              <div class="carousel-item">
-                                <img class="d-block w-100"
-                                    src="../assets/img/Event/HCiD/AFD_7555-min.JPG"
-                                    alt="Five slide">
-                            </div>
-                            <div class="carousel-item">
-                              <img class="d-block w-100"
-                                  src="../assets/img/Event/HCiD/AFD_7566-min.JPG"
-                                  alt="Five slide">
-                          </div>
-                          <div class="carousel-item">
-                            <img class="d-block w-100"
-                                src="../assets/img/Event/HCiD/AFD_7574-min.JPG"
-                                alt="Five slide">
-                        </div>
-                        <div class="carousel-item">
-                          <img class="d-block w-100"
-                              src="../assets/img/Event/HCiD/AFD_7594-min.JPG"
-                              alt="Five slide">
-                      </div>
-                      <div class="carousel-item">
-                        <img class="d-block w-100"
-                            src="../assets/img/Event/HCiD/AFD_7604-min.JPG"
-                            alt="Five slide">
-                    </div>
-                    <div class="carousel-item">
-                      <img class="d-block w-100"
-                          src="../assets/img/Event/HCiD/AFD_7620-min.JPG"
-                          alt="Five slide">
-                  </div>
-                  <div class="carousel-item">
-                    <img class="d-block w-100"
-                        src="../assets/img/Event/HCiD/AFD_7626-min.JPG"
-                        alt="Five slide">
-                </div>
-=======
                                         src="../AFD/assets/img/Event/HCiD/AFD_7546-min.JPG"
                                         alt="Five slide">
                                 </div>
@@ -200,7 +127,6 @@
                                         src="../AFD/assets/img/Event/HCiD/AFD_7626-min.JPG"
                                         alt="Five slide">
                                 </div>
->>>>>>> c7f02b2256662a67b3105bd4386ad783400e1d1a
                             </div>
                             <a class="carousel-control-prev"
                                 href="#carouselExampleIndicators"
@@ -233,34 +159,6 @@
                         data-ride="carousel">
                         <div class="carousel-inner">
                             <div class="carousel-item active">
-<<<<<<< HEAD
-                                <img class="d-block w-100" src="../assets/img/Event/Kabadi/AH7A6059-min.JPG" alt="First
-                                    slide">
-                            </div>
-                            <div class="carousel-item">
-                                <img class="d-block w-100" src="../assets/img/Event/Kabadi/AH7A6066-min.JPG" alt="Second
-                                    slide">
-                            </div>
-                            <div class="carousel-item">
-                                <img class="d-block w-100" src="../assets/img/Event/Kabadi/AH7A6069-min.JPG" alt="Third
-                                    slide">
-                            </div>
-                            <div class="carousel-item">
-                                <img class="d-block w-100" src="../assets/img/Event/Kabadi/AH7A6454-min.JPG" alt="Third
-                                    slide">
-                            </div>
-                            <div class="carousel-item">
-                                <img class="d-block w-100" src="../assets/img/Event/Kabadi/AH7A6531-min.JPG" alt="Third
-                                    slide">
-                            </div>
-                            <div class="carousel-item">
-                                <img class="d-block w-100" src="../assets/img/Event/Kabadi/DSC_7244-min.JPG" alt="Third
-                                    slide">
-                            </div>
-                            <div class="carousel-item">
-                                <img class="d-block w-100" src="../assets/img/Event/Kabadi/DSC_7250-min.JPG" alt="Third
-                                    slide">
-=======
                                 <img class="d-block w-100"
                                     src="../AFD/assets/img/Event/Kabadi/AH7A6059-min.JPG"
                                     alt="First
@@ -372,7 +270,6 @@
                                         COMPETITION- 2021</h5>
 
                                 </div>
->>>>>>> c7f02b2256662a67b3105bd4386ad783400e1d1a
                             </div>
                         </div>
                         <a class="carousel-control-prev"
@@ -395,12 +292,6 @@
             </div>
             <div class="col-lg-6 col-md-12">
                 <div class="about-content">
-<<<<<<< HEAD
-                   
-                    <h5>MUJIB BORSHO INTER SERVICE KABADI COMPETITION – 2021</h5>
-                    <p>'Mujib Borsho Inter Service Kabadi Competition-2021’ was held from 29 November 2021 to 02 December 2021 at BAF Base Birsreshtho Matiur Rahman, Jashore. Bangladesh Army, Bangladesh Navy and Bangladesh Air Force Kabadi teams participated in the competition. Rear Admiral M Anwar Hossain (ND), NGP, ndc, afwc, psc, BN, Commander Khulna Naval Area inaugurated the competition on 29 November 2021. Bangladesh Navy became Champion and Bangladesh Air Force became Runner-up. M Tuhin Tarafdar, L Top, Captain of Bangladesh Navy team adjudged as the best player of the Competition.  </p>
-                    <p> Air Vice Marshal A H M Fazlul Haque, BBP, BSP, ndu, afwc, psc, Assistant Chief of Air Staff (Plans) Bangladesh Air Force handed over the trophies to the winner, runner-up and the best player as Chief Guest of the Closing Ceremony.
-=======
 
                     <h5 style="text-align: justify;">MUJIB BORSHO INTER SERVICE
                         KABADI COMPETITION – 2021</h5>
@@ -422,17 +313,135 @@
                         Bangladesh Air Force handed over the trophies to the
                         winner, runner-up and the best player as Chief Guest of
                         the Closing Ceremony.
->>>>>>> c7f02b2256662a67b3105bd4386ad783400e1d1a
 
                     </p>
                 </div>
             </div>
         </div>
     </div>
-<<<<<<< HEAD
-   <br>
-=======
     <br>
->>>>>>> c7f02b2256662a67b3105bd4386ad783400e1d1a
+    <!-- new 3 -->
+    <section class="our-coverage-area ptb-100">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-6 col-md-12">
+                    <div class="coverage-area-content">
+                        <!-- <span class="sub-title"><i
+                                class="flaticon-care-about-environment"></i>
+                            About HCiD</span> -->
+                        <h2 style="text-align: justify;">International Day of Military Sports-2022</h2>
+                        <p style="text-align: justify;">Member of Bangladesh Armed Forces celebrated  “International Day of Military Sports” on 18 February 2022. Director General Training of Armed Forces Division, Air Commodore S M Muied Hossain, BSP, rcds, afwc. psc, GD (P) inaugurated the event. members of different ranks from   Armed Forces Division, Bangladesh Army, Bangladesh Navy and Bangladesh Air Force participated in rally and jogging for celebrating the day. </p>
+                       
+
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-12">
+                    <div class="coverage-area-map">
+                        <div id="carouselExampleIndicators" class="carousel
+                            slide"
+                            data-ride="carousel">
+                            <!-- <ol class="carousel-indicators">
+                                <li data-target="#carouselExampleIndicators"
+                                    data-slide-to="0"
+                                    class="active"></li>
+                                <li data-target="#carouselExampleIndicators"
+                                    data-slide-to="1"></li>
+                                <li data-target="#carouselExampleIndicators"
+                                    data-slide-to="2"></li>
+                                    <li data-target="#carouselExampleIndicators"
+                                    data-slide-to="3"></li>
+                                    <li data-target="#carouselExampleIndicators"
+                                    data-slide-to="4"></li>
+                            </ol> -->
+                            <div class="carousel-inner2">
+                                <div class="carousel-item active">
+                                    <img class="d-block w-100"
+                                        src="../AFD/assets/img/Event/Military/DSC_1221.JPG"
+                                        alt="First slide">
+                                </div>
+                                <div class="carousel-item ">
+                                    <img class="d-block w-100"
+                                        src="../AFD/assets/img/Event/Military/DSC_1224.JPG"
+                                        alt="Second slide">
+                                </div>
+                                <div class="carousel-item">
+                                    <img class="d-block w-100"
+                                        src="../AFD/assets/img/Event/HCiD/AFD_7482-min.JPG"
+                                        alt="Third slide">
+                                </div>
+                                <div class="carousel-item">
+                                    <img class="d-block w-100"
+                                        src="../AFD/assets/img/Event/HCiD/AFD_7520-min.JPG"
+                                        alt="Four slide">
+                                </div>
+                                <div class="carousel-item">
+                                    <img class="d-block w-100"
+                                        src="../AFD/assets/img/Event/HCiD/AFD_7546-min.JPG"
+                                        alt="Five slide">
+                                </div>
+                                <div class="carousel-item">
+                                    <img class="d-block w-100"
+                                        src="../AFD/assets/img/Event/HCiD/AFD_7550-min.JPG"
+                                        alt="Five slide">
+
+                                </div>
+                                <div class="carousel-item">
+                                    <img class="d-block w-100"
+                                        src="../AFD/assets/img/Event/HCiD/AFD_7555-min.JPG"
+                                        alt="Five slide">
+                                </div>
+                                <div class="carousel-item">
+                                    <img class="d-block w-100"
+                                        src="../AFD/assets/img/Event/HCiD/AFD_7566-min.JPG"
+                                        alt="Five slide">
+                                </div>
+                                <div class="carousel-item">
+                                    <img class="d-block w-100"
+                                        src="../AFD/assets/img/Event/HCiD/AFD_7574-min.JPG"
+                                        alt="Five slide">
+                                </div>
+                                <div class="carousel-item">
+                                    <img class="d-block w-100"
+                                        src="../AFD/assets/img/Event/HCiD/AFD_7594-min.JPG"
+                                        alt="Five slide">
+                                </div>
+                                <div class="carousel-item">
+                                    <img class="d-block w-100"
+                                        src="../AFD/assets/img/Event/HCiD/AFD_7604-min.JPG"
+                                        alt="Five slide">
+                                </div>
+                                <div class="carousel-item">
+                                    <img class="d-block w-100"
+                                        src="../AFD/assets/img/Event/HCiD/AFD_7620-min.JPG"
+                                        alt="Five slide">
+                                </div>
+                                <div class="carousel-item">
+                                    <img class="d-block w-100"
+                                        src="../AFD/assets/img/Event/HCiD/AFD_7626-min.JPG"
+                                        alt="Five slide">
+                                </div>
+                            </div>
+                            <a class="carousel-control-prev"
+                                href="#carouselExampleIndicators"
+                                role="button" data-slide="prev">
+                                <span class="carousel-control-prev-icon"
+                                    aria-hidden="true"></span>
+                                <span class="sr-only">Previous</span>
+                            </a>
+                            <a class="carousel-control-next"
+                                href="#carouselExampleIndicators"
+                                role="button" data-slide="next">
+                                <span class="carousel-control-next-icon"
+                                    aria-hidden="true"></span>
+                                <span class="sr-only">Next</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <br>
+    <!-- new 3 end -->
 
     <?php include 'footer.php'; ?>
